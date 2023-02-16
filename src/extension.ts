a66e5370632949bd5aa01049653eab47f1fd3ad3
Leonardo Montini <lion.48m@gmail.com>
2023-02-16 21:21:22 +0100

    Closed tabs properly with the vscode tabGroups.close API

--- src/extension.ts
+++ src/extension.ts
@@ -1,77 +1,62 @@
 import * as vscode from "vscode";
-import { GitExtension, Repository } from "./git";
+import { Change, GitExtension, Repository } from "./git";
 
 export function activate(context: vscode.ExtensionContext) {
   const closeCommand = vscode.commands.registerCommand(
     "close-tabs.git-unchanged",
     async () => {
-      const gitExtension =
-        vscode.extensions.getExtension<GitExtension>("vscode.git")?.exports;
-      if (!gitExtension) {
-        console.log("Git extension not found");
-        return;
-      }
+      try {
+        const gitExtension =
+          vscode.extensions.getExtension<GitExtension>("vscode.git")?.exports;
+        if (!gitExtension) {
+          console.log("Git extension not found");
+          return;
+        }
 
-      const repositories = gitExtension.getAPI(1).repositories;
-      if (!repositories.length) {
-        console.log("No repositories found");
-        return;
-      }
+        const repositories = gitExtension.getAPI(1).repositories;
+        if (!repositories.length) {
+          console.log("No repositories found");
+          return;
+        }
 
-      const targetFiles = vscode.window.tabGroups.all
-        .flatMap((tabGroup) => tabGroup.tabs)
-        .filter((tab) => {
-          if (tab.isPinned) {
-            // TODO: Add config to allow closing pinned tabs
-            return false;
-          }
-
-          if (
-            tab.input instanceof vscode.TabInputText ||
-            tab.input instanceof vscode.TabInputCustom ||
-            tab.input instanceof vscode.TabInputNotebook
-          ) {
-            const fileName = tab.input.uri.fsPath;
-            if (!fileName) {
-              return true;
+        vscode.window.tabGroups.all
+          .flatMap((tabGroup) => tabGroup.tabs)
+          .forEach((tab) => {
+            if (tab.isPinned) {
+              // TODO: Add config to allow closing pinned tabs
+              return;
             }
 
-            const canBeClosed = repositories.some(
-              (repository) => !hasDiff(fileName, repository)
-            );
+            if (
+              tab.input instanceof vscode.TabInputText ||
+              tab.input instanceof vscode.TabInputCustom ||
+              tab.input instanceof vscode.TabInputNotebook
+            ) {
+              // find if file is changed in any of the repositories
 
-            return canBeClosed;
-          }
+              const fileName = tab.input.uri.fsPath;
+              if (!fileName) {
+                return;
+              }
 
-          return true;
-        })
-        .map((tab) => {
-          if (
-            tab.input instanceof vscode.TabInputText ||
-            tab.input instanceof vscode.TabInputCustom ||
-            tab.input instanceof vscode.TabInputNotebook
-          ) {
-            return tab.input.uri as vscode.Uri;
-          } else if (
-            tab.input instanceof vscode.TabInputTextDiff ||
-            tab.input instanceof vscode.TabInputNotebookDiff
-          ) {
-            return tab.input.original as vscode.Uri;
-          }
-          return undefined;
-        });
+              const canBeClosed = repositories.some(
+                (repository) => !hasDiff(fileName, repository)
+              );
 
-      for (const uri of targetFiles) {
-        if (!uri) {
-          continue;
-        }
-        // https://stackoverflow.com/questions/44733028/how-to-close-textdocument-in-vs-code
-        await vscode.window.showTextDocument(uri, {
-          preview: true,
-          preserveFocus: false,
-        });
-
-        vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+              if (canBeClosed) {
+                vscode.window.tabGroups.close(tab, true);
+              }
+            } else if (
+              tab.input instanceof vscode.TabInputTextDiff ||
+              tab.input instanceof vscode.TabInputNotebookDiff
+            ) {
+              vscode.window.tabGroups.close(tab, true);
+            }
+          });
+      } catch (error) {
+        vscode.window.showErrorMessage(
+          "Unexpected error while closing the tabs!"
+        );
       }
     }
   );
@@ -80,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function hasDiff(fileName: string, repository: Repository) {
-  return repository.state.workingTreeChanges.some((change: any) => {
+  return repository.state.workingTreeChanges.some((change: Change) => {
     return change.uri.fsPath === fileName;
   });
 }
