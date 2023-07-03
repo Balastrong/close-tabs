3cb1a24aceebdd60b16956882bd0e41229d6868d
Leonardo Montini <lion.48m@gmail.com>
2023-07-03 18:53:56 +0200

    Move closeEditorsToTheLeft as custom command (#16)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import { closeUnchanged } from "./commands/closeUnchanged";
 import { Change, GitExtension, Repository } from "./git";
+import { closeLeft } from "./commands/closeLeft";
 
 export function activate(context: vscode.ExtensionContext) {
   const closeUnchangedCmd = vscode.commands.registerCommand(
@@ -8,5 +9,11 @@ export function activate(context: vscode.ExtensionContext) {
     closeUnchanged
   );
 
+  const closeLeftCmd = vscode.commands.registerCommand(
+    "close-tabs.close-left",
+    closeLeft
+  );
+
   context.subscriptions.push(closeUnchangedCmd);
+  context.subscriptions.push(closeLeftCmd);
 }
