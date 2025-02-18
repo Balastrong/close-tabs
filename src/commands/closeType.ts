8ef02b1cbff16ff389b34c47e63fecfc7550bc95
Ian A. MacMillan <macmillan@caltech.edu>
2025-02-18 10:53:59 -0800

    feat: add close by file extension (#23)

---
+++ src/commands/closeType.ts
@@ -0,0 +1,66 @@
+import * as vscode from "vscode";
+
+export const closeType = async (uri?: vscode.Uri) => {
+    try {
+        const config = vscode.workspace.getConfiguration("closeTabs");
+        let extensionsToClose: string[] = config.get("extensionsToClose", []);
+        const caseSensitive: boolean = config.get("extensionsCaseSensitive", true);
+
+        console.log("Starting to close tabs based on type...");
+
+        // If no extensions are specified in the settings, use the extension of the right-clicked file
+        if (extensionsToClose.length === 0 && uri) {
+            const fileExtension = uri.fsPath.split('.').pop();
+            if (fileExtension) {
+                extensionsToClose = [fileExtension];
+                console.log(`No extensions specified in settings. Using the extension of the right-clicked file: ${fileExtension}`);
+            }
+        }
+
+        // Normalize extensions to remove leading dots
+        const normalizedExtensionsToClose = extensionsToClose.map(ext => ext.startsWith('.') ? ext.slice(1) : ext);
+        const finalExtensionsToClose = caseSensitive ? normalizedExtensionsToClose : normalizedExtensionsToClose.map(ext => ext.toLowerCase());
+
+        vscode.window.tabGroups.all
+            .flatMap((tabGroup) => tabGroup.tabs)
+            .forEach((tab) => {
+                if (tab.isPinned && !config.allowClosePinnedTabs) {
+                    console.log(`Skipping pinned tab: ${tab.label}`);
+                    return;
+                }
+
+                let fileName: string | undefined;
+
+                if (tab.input instanceof vscode.TabInputText) {
+                    fileName = tab.input.uri.fsPath;
+                } else if (tab.input instanceof vscode.TabInputCustom) {
+                    fileName = tab.input.uri.fsPath;
+                }
+
+                if (!fileName) {
+                    console.log("No file name found for tab.");
+                    return;
+                }
+
+                console.log(`Checking file: ${fileName}`);
+
+                // Check if the file extension is in the list of extensions to close
+                const fileExtension = fileName.split('.').pop();
+                if (fileExtension) {
+                    const normalizedExtension = caseSensitive ? fileExtension : fileExtension.toLowerCase();
+
+                    if (finalExtensionsToClose.includes(normalizedExtension)) {
+                        console.log(`Closing tab with file: ${fileName}`);
+                        vscode.window.tabGroups.close(tab, true);
+                    } else {
+                        console.log(`File extension is not in the list to close: ${fileName}`);
+                    }
+                }
+            });
+
+        console.log("Finished closing tabs.");
+    } catch (error) {
+        vscode.window.showErrorMessage("Unexpected error while closing the tabs!");
+        console.error("Error while closing tabs:", error);
+    }
+};
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import { closeUnchanged } from "./commands/closeUnchanged";
 import { Change, GitExtension, Repository } from "./git";
 import { closeLeft } from "./commands/closeLeft";
+import { closeType } from "./commands/closeType"; 
 
 export function activate(context: vscode.ExtensionContext) {
   const closeUnchangedCmd = vscode.commands.registerCommand(
@@ -14,6 +15,12 @@ export function activate(context: vscode.ExtensionContext) {
     closeLeft
   );
 
+  const closeTypeCmd = vscode.commands.registerCommand(
+    "close-tabs.closeType",
+    (uri: vscode.Uri) => closeType(uri)
+  );
+
   context.subscriptions.push(closeUnchangedCmd);
   context.subscriptions.push(closeLeftCmd);
+  context.subscriptions.push(closeTypeCmd);
 }
